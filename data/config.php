3ea29b2f29a96f9d16f021ca4de1dbe788640cc8
r321394703 <2547977230@qq.com>
2021-06-30 15:24:45 +0800

    fix: 初始化 ecshop

---
+++ data/config.php
@@ -0,0 +1,37 @@
+<?php
+// database host
+$db_host   = "localhost:3306";
+
+// database name
+$db_name   = "ecshop";
+
+// database username
+$db_user   = "ecshop";
+
+// database password
+$db_pass   = "123456789";
+
+// table prefix
+$prefix    = "ecs_";
+
+$timezone    = "PRC";
+
+$cookie_path    = "/";
+
+$cookie_domain    = "";
+
+$session = "1440";
+
+define('EC_CHARSET','utf-8');
+
+define('ADMIN_PATH','admin');
+
+define('AUTH_KEY', 'this is a key');
+
+define('OLD_AUTH_KEY', '');
+
+define('API_TIME', '');
+
+define('STORE_KEY','7852c7c2ebb5ebeb3952e06be6f8ca77');
+
+?>
